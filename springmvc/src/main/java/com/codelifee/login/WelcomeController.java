60a070eb56e9d1a531cf8a06a8f42d2ee13aa6de
codelifee <suminjung09@gmail.com>
2020-11-18 11:42:24 +0900

    added securityConfiguration filter

---
+++ springmvc/src/main/java/com/codelifee/login/WelcomeController.java
@@ -0,0 +1,18 @@
+package com.codelifee.login;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class WelcomeController {	
+	
+	@RequestMapping(value="/", method= RequestMethod.GET)
+	public String handleWelcome(ModelMap model) {
+		model.put("name", "codelifee");
+		return "welcome";
+	}
+	
+	
+}
